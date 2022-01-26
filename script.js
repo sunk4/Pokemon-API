22f8a2b6a8421855a43b808aa599d282f81cb13a
Roman Trnka <trnka.roman13@gmail.com>
2022-01-26 13:35:50 +0100

    used async, await, try and catch for error

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const btn = document
   .querySelector("[data-btn]")
   .addEventListener("click", findWinner)
 
-function findWinner() {
+async function findWinner() {
   const pokemonOne = document
     .querySelector("[data-pokemon1]")
     .value.toLowerCase()
@@ -11,64 +11,66 @@ function findWinner() {
     .querySelector("[data-pokemon2]")
     .value.toLowerCase()
     .trim()
+
   const url1 = `https://pokeapi.co/api/v2/pokemon/${pokemonOne}/`
   const url2 = `https://pokeapi.co/api/v2/pokemon/${pokemonTwo}/`
   let weightPokemon1 = []
   let weightPokemon2 = []
 
-  fetch(url1)
-    .then((res) => res.json())
-    .then((data) => {
-      weightPokemon1.push(data.stats[0].base_stat)
-      document.querySelector("[data-name1]").innerText = `Name: ${data.name}`
-      document.querySelector("[data-img1]").src = data.sprites.front_default
+  try {
+    const res = await fetch(url1)
+    const data = await res.json()
+
+    weightPokemon1.push(data.stats[0].base_stat)
+    document.querySelector("[data-name1]").innerText = `Name: ${data.name}`
+    document.querySelector("[data-img1]").src = data.sprites.front_default
+    document.querySelector(
+      "[data-ability11]"
+    ).innerText = `Ability ${data.abilities[0].ability.name}`
+    document.querySelector(
+      "[data-ability12]"
+    ).innerText = `Ability ${data.abilities[1].ability.name}`
+    document.querySelector(
+      "[data-type1]"
+    ).innerText = `Type: ${data.types[0].type.name}`
+    document.querySelector(
+      "[data-weight1]"
+    ).innerText = `Weight: ${data.weight} Height: ${data.height}`
+  } catch (err) {
+    console.log(`error ${err}`)
+  }
+
+  try {
+    const res2 = await fetch(url2)
+    const data2 = await res2.json()
+
+    weightPokemon2.push(data2.stats[0].base_stat)
+    document.querySelector("[data-name2]").innerText = `Name: ${data2.name}`
+    document.querySelector("[data-img2]").src = data2.sprites.front_default
+    document.querySelector(
+      "[data-ability21]"
+    ).innerText = `Ability ${data2.abilities[0].ability.name}`
+    document.querySelector(
+      "[data-ability22]"
+    ).innerText = `Ability ${data2.abilities[1].ability.name}`
+    document.querySelector(
+      "[data-type2]"
+    ).innerText = `Type: ${data2.types[0].type.name}`
+    document.querySelector(
+      "[data-weight2]"
+    ).innerText = `Weight: ${data2.weight} Height: ${data2.height}`
+    if (weightPokemon1[0] === weightPokemon2[0]) {
+      document.querySelector("[data-result]").innerText = `Equal hp`
+    } else if (weightPokemon1[0] > weightPokemon2[0]) {
       document.querySelector(
-        "[data-ability11]"
-      ).innerText = `Ability ${data.abilities[0].ability.name}`
+        "[data-result]"
+      ).innerText = `First pokemon have more hp`
+    } else {
       document.querySelector(
-        "[data-ability12]"
-      ).innerText = `Ability ${data.abilities[1].ability.name}`
-      document.querySelector(
-        "[data-type1]"
-      ).innerText = `Type: ${data.types[0].type.name}`
-      document.querySelector(
-        "[data-weight1]"
-      ).innerText = `Weight: ${data.weight} Height: ${data.height}`
-    })
-  fetch(url2)
-    .then((res) => res.json())
-    .then((data) => {
-      weightPokemon2.push(data.stats[0].base_stat)
-      document.querySelector("[data-name2]").innerText = `Name: ${data.name}`
-      document.querySelector("[data-img2]").src = data.sprites.front_default
-      document.querySelector(
-        "[data-ability21]"
-      ).innerText = `Ability ${data.abilities[0].ability.name}`
-      document.querySelector(
-        "[data-ability22]"
-      ).innerText = `Ability ${data.abilities[1].ability.name}`
-      document.querySelector(
-        "[data-type2]"
-      ).innerText = `Type: ${data.types[0].type.name}`
-      document.querySelector(
-        "[data-weight2]"
-      ).innerText = `Weight: ${data.weight} Height: ${data.height}`
-      if (weightPokemon1[0] === weightPokemon2[0]) {
-        document.querySelector("[data-result]").innerText = `Equal hp`
-      } else if (weightPokemon1[0] > weightPokemon2[0]) {
-        document.querySelector(
-          "[data-result]"
-        ).innerText = `First pokemon have more hp`
-      } else {
-        document.querySelector(
-          "[data-result]"
-        ).innerText = `First pokemon have less hp`
-      }
-    })
-    .catch((err) => {
-      console.log(`error ${err}`)
-    })
-    .catch((err) => {
-      console.log(`error ${err}`)
-    })
+        "[data-result]"
+      ).innerText = `First pokemon have less hp`
+    }
+  } catch (err) {
+    console.log(`error ${err}`)
+  }
 }
